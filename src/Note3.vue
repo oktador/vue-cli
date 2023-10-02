da33ef0a717fad670b489da7efc08266008cdd32
oktador <oktay.doruk@verion.com.tr>
2023-10-03 00:36:02 +0300

    note3 commited

---
+++ src/Note3.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="title">{{ title }}</h1>
+  <br />
+  <input type="text" ref="input1" />
+  <button @click="onButtonClick">Click Me To Focus On Input</button>
+</template>
+
+<script>
+export default {
+  name: "Note3",
+  data() {
+    return {
+      title: "Note 3",
+    };
+  },
+  methods: {
+    onButtonClick() {
+      this.$refs.input1.focus();
+    },
+  },
+};
+</script>
