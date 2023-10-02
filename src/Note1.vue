876575c63263aac5adbc35834f06110cb99ba4cd
oktador <oktay.doruk@verion.com.tr>
2023-10-03 00:24:36 +0300

    note1 commited

---
+++ src/Note1.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="title">{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: "Note1",
+  data() {
+    return {
+      title: "Note 1",
+    };
+  },
+};
+</script>
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import Note from "./Note1.vue";
+import "./assets/stylesheets/style.css";
+
+createApp(Note).mount("#app");
